73db81e3c5662cd1c7f70744f6586292c2ddd212
kartikpande123 <kartikpande67@gmail.com>
2025-06-04 14:18:36 +0530

    fix

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // if you use cookies/auth headers
 }));
+app.options('*', cors()); // enable pre-flight for all routes
 
 
 //Multer for file uploads 
